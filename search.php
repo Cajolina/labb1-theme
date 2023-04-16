cb41ccc5c54356d78339dcadea8432bd1b1e7b1e
Carro <carolinaisabellaandersson@gmail.com>
2023-04-16 18:02:29 +0200

    creating a search page

--- search.php
+++ search.php
@@ -1,12 +1,56 @@
-<?php get_header() ?>
+<?php
+//Hämtar allt innehåll i header.php
+get_header() ?>
 
-<h1><?php echo "Sökresultat för: ";
-    the_search_query() ?></h1>
 
-<?php if (have_posts()) : while (have_posts()) : the_post() ?>
-        <?php get_template_part('/template-parts/content-archive') ?>
+<main>
+    <section>
+        <div class="container">
+            <div class="row">
+                <div id="primary" class="col-xs-12 col-md-8 col-md-offset-2">
 
-<?php endwhile;
-endif; ?>
+                    <!-- Hämtar titel för sidan -->
+                    <h1><?php wp_title('') ?></h1>
+                    <div class="searchform-wrap">
+                        <form id="searchform" class="searchform">
+                            <div>
+                                <label> <?php dynamic_sidebar('SearchWidget-2') ?></label>
 
-<?php get_footer() ?>
+                                <!-- Visar sökformuläret -->
+                                <?php get_search_form(); ?>
+                            </div>
+                        </form>
+                    </div>
+
+
+
+                    <?php
+                    //Loopar ut posterna och hämtar innehåll från templateparts
+                    if (have_posts()) {
+                        while (have_posts()) {
+                            the_post();
+
+                            get_template_part('/template-parts/content-archive');
+                        }
+                    } else {
+                        // Visar meddelande för tomma sökresultat
+                        echo "Tyvärr hittade inget sökresultat för: \"" . get_search_query() . "\"";
+                    }
+                    ?>
+
+
+
+                    <nav class="navigation pagination">
+                        <h2 class="screen-reader-text">Inläggsnavigering</h2>
+                        <!-- Anropar inbyggd funktion som genererar paginerade länkar-->
+                        <?php the_posts_pagination() ?>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
+
+<?php
+//Hämtar allt innehåll i footer.php
+get_footer() ?>
